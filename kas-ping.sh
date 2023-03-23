013f780dda95ccf262f5758d7175e03938b12237
William <634206396@qq.com>
2023-03-23 17:43:04 +0800

    Update kas-ping.sh

--- kas-ping.sh
+++ kas-ping.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 
-LOGPATH="./kas-ping.log"
 TARGET="kas.gpumine.org"
+LOGPATH="./kas-ping.log"
+GATEWAY=`ip route | awk '/^default/ {print $3}'`
+
+ip_list=($TARGET $GATEWAY)
 
 while true; do
-echo -n `date` "---> ">> $LOGPATH; 
-(ping $TARGET -c 1 -W 1|grep "from">> $LOGPATH)  && (echo -n "" >> $LOGPATH) || (echo "Fail.">> $LOGPATH)
-
-sleep 1
+  for ip in "${ip_list[@]}"; do
+    echo -n `date` "---> $ip ">> $LOGPATH; 
+    (ping $ip -c 1 -W 1 | grep "from" >> $LOGPATH) && (echo -n "" >> $LOGPATH) || (echo "Fail." >> $LOGPATH)
+  done
+  sleep 1
 done
