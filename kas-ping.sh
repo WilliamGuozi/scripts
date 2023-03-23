83f39c8170c73895eda0be8fc5e781bbbf2f0dc5
William <634206396@qq.com>
2023-03-23 16:43:43 +0800

    Add files via upload

---
+++ kas-ping.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+LOGPATH="./ping.log"
+TARGET="kas.gpumine.org"
+
+while true; do
+echo -n `date` "---> ">> $LOGPATH; 
+(ping $TARGET -c 1 -W 1|grep "from">> $LOGPATH)  && (echo -n "" >> $LOGPATH) || (echo "Fail.">> $LOGPATH)
+
+sleep 1
+done
